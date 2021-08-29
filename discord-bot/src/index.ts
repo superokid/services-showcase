3d884fc286a5043b638f881840a2c1e01cdbc24a
Kelvin Hong <kelvinok52@gmail.com>
2021-08-29 16:15:06 +0700

    [discord-bot] init bot listener

--- discord-bot/src/index.ts
+++ discord-bot/src/index.ts
@@ -1,3 +1,18 @@
-const msg = 'Hello World!';
+import dotenv from 'dotenv';
+import { Client, Intents } from 'discord.js';
+dotenv.config();
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { DISCORD_TOKEN } = process.env;
 
-console.log(msg);
+bot.login(DISCORD_TOKEN);
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user?.tag}!`);
+});
+
+bot.on('message', (msg) => {
+  console.log(msg);
+  if (msg.content === '!hi') {
+    msg.reply('hallooo');
+  }
+});
