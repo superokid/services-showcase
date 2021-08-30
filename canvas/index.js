81e8ba35ab998c640585d7cbc4efbab8a3ac2ba5
Kelvin Hong <kelvinok52@gmail.com>
2021-08-30 21:06:19 +0700

    [canvas] cors

--- canvas/index.js
+++ canvas/index.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const routes = require('./routes');
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.use('/v1', routes);
 
