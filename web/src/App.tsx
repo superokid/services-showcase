331eac49bc267098bd4bb0d53fd716eb41c48666
Kelvin Hong <kelvinok52@gmail.com>
2021-08-30 19:52:16 +0700

    [web] init

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Leaderboard from './pages/Leaderboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Leaderboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/pages/Leaderboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {}
+
+const Leaderboard = (props: Props) => {
+  return <div>Leaderboard</div>;
+};
+
+export default Leaderboard;
